4e7a62d5c2e417d44bb79b45b71c9c2342e637f5
lcarbonnell <llawencarbonnell@gmail.com>
2017-09-26 09:28:18 +0200

    affiche la draisienne dans le catalogue de jouet

---
+++ jouet.js
@@ -0,0 +1,9 @@
+class Jouet{
+    constructor(libelle){
+        this._libelle = libelle;
+    }
+    get libelle(){
+        return this._libelle;
+    }
+}
+module.exports = Jouet;
--- serveur.js
+++ serveur.js
@@ -1,1 +1,4 @@
-console.log("hello world");
+const Jouet =require('./Jouet.js');
+console.log('bienvenue dans le catalogue de jouet');
+var unJouet = new Jouet ("draisienne junior bleue")
+console.log(unJouet.libelle);
